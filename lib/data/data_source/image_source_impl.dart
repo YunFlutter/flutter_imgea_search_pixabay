26fc3eed4a47626806b964f09cfc5bb374094a30
YUNSEO <og737596@gmail.com>
2025-04-17 16:28:32 +0900

    feat : dataSource getImage 완성

---
+++ lib/data/data_source/image_source_impl.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:flutter_imgea_search_pixabay/core/env_file/api_key.dart';
+import 'package:flutter_imgea_search_pixabay/core/result/result.dart';
+import 'package:flutter_imgea_search_pixabay/data/data_source/image_source.dart';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+class ImageSourceImpl implements ImageSource {
+  @override
+  Future<Result<Map<String, dynamic>, String>> getImage() async {
+    try{
+      final Response response = await http.get(
+        Uri.parse('https://pixabay.com/api/${ApiKey().pixabayAPI}&lang=ko&image_type=photo'),
+      );
+      return Result.success(jsonDecode(response.body));
+    }catch (e){
+      return Result.error('GetImage 실패 error: $e');
+    }
+
+  }
+}
